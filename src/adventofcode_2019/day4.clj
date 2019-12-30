7490d628894cf71e024fb31fa4fc952b5f5a08c4
Tejas Bubane <tejasbubane@gmail.com>
2019-12-30 17:10:16 +0530

    Use partition for day4 part1
    Way cleaner solution now!

--- src/adventofcode_2019/day4.clj
+++ src/adventofcode_2019/day4.clj
@@ -2,22 +2,20 @@
   (:gen-class))
 
 ;; Part 1
+(defn pairs [digits]
+  (partition 2 1 digits))
+
 (defn digits [number]
   (map #(Character/digit % 10) (str number)))
 
 (defn has-same-adjacent [digits]
-  (and (>= (count digits) 2)
-       (or (= (first digits) (first (rest digits)))
-           (has-same-adjacent (rest digits)))))
+  (some (fn [[a b]] (= a b)) digits))
 
 (defn non-decreasing [digits]
-  (or (empty? digits)
-      (= (count digits) 1)
-      (and (<= (first digits) (first (rest digits)))
-           (non-decreasing (rest digits)))))
+  (every? (fn [[a b]] (<= a b)) digits))
 
 (defn possible-passwords [start end]
   (filter (fn [num]
-            (let [ds (digits num)]
+            (let [ds (pairs (digits num))]
               (and (non-decreasing ds) (has-same-adjacent ds))))
           (range start (inc end))))
--- test/adventofcode_2019/day4_test.clj
+++ test/adventofcode_2019/day4_test.clj
@@ -4,15 +4,15 @@
 
 ;; Part 1
 (deftest test-has-same-adjacent
-  (testing "1 2 1" (is (not (has-same-adjacent '(1 2 1)))))
-  (testing "2 2" (is (has-same-adjacent '(2 2))))
-  (testing "1 3 3 1 2" (is (has-same-adjacent '(1 3 3 1 2))))
-  (testing "1 3 4 5 6 4" (is (not (has-same-adjacent '(1 3 4 5 6 4))))))
+  (testing "1 2 1" (is (not (has-same-adjacent (pairs '(1 2 1))))))
+  (testing "2 2" (is (has-same-adjacent (pairs '(2 2)))))
+  (testing "1 3 3 1 2" (is (has-same-adjacent (pairs '(1 3 3 1 2)))))
+  (testing "1 3 4 5 6 4" (is (not (has-same-adjacent (pairs '(1 3 4 5 6 4)))))))
 
 (deftest test-non-decreasing
-  (testing "1 2 1" (is (not (non-decreasing '(1 2 1)))))
-  (testing "2 2" (is (non-decreasing '(2 2))))
-  (testing "1 3 8 9" (is (non-decreasing '(1 3 8 9)))))
+  (testing "1 2 1" (is (not (non-decreasing (pairs '(1 2 1))))))
+  (testing "2 2" (is (non-decreasing (pairs '(2 2)))))
+  (testing "1 3 8 9" (is (non-decreasing (pairs '(1 3 8 9))))))
 
 (deftest test-puzzle-input
   (print "Part 1: ")
