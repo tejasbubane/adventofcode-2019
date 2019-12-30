4c927d7e3670c9cc6ff34f2b0ce3ab4db508ffc0
Tejas Bubane <tejasbubane@gmail.com>
2019-12-30 23:53:51 +0530

    Refactor day-4 to common meta function
    For both parts 1 & 2

--- src/adventofcode_2019/day4.clj
+++ src/adventofcode_2019/day4.clj
@@ -14,11 +14,20 @@
 (defn non-decreasing [digits]
   (every? (fn [[a b]] (<= a b)) digits))
 
-(defn possible-passwords [start end]
-  (filter (fn [num]
-            (let [ds (pairs (digits num))]
-              (and (non-decreasing ds) (has-same-adjacent ds))))
-          (range start (inc end))))
+(defn- meta-passwords [& conditions]
+  (fn [start end]
+    (let [cond1 (first conditions)
+          cond2 (second conditions)
+          all-digits (map (comp pairs digits) (range start (inc end)))]
+      (filter (fn [ds] (and (cond1 ds) (cond2 ds))) all-digits))))
+
+(def possible-passwords (meta-passwords non-decreasing has-same-adjacent))
+
+;; (defn possible-passwords [start end]
+;;   (filter (fn [num]
+;;             (let [ds (pairs (digits num))]
+;;               (and (non-decreasing ds) (has-same-adjacent ds))))
+;;           (range start (inc end))))
 
 (defn- all-pairs [digits]
   (filter (fn [[a b]] (= a b)) digits))
@@ -26,9 +35,11 @@
 (defn- not-part-of-larger-group [digits]
   (some (fn [[d c]] (= c 1)) (seq (frequencies (all-pairs digits)))))
 
-(defn possible-passwords-2 [start end]
-  (filter (fn [num]
-            (let [ds (pairs (digits num))]
-              (and (non-decreasing ds)
-                   (not-part-of-larger-group ds))))
-          (range start (inc end))))
+(def possible-passwords-2 (meta-passwords non-decreasing not-part-of-larger-group))
+
+;; (defn possible-passwords-2 [start end]
+;;   (filter (fn [num]
+;;             (let [ds (pairs (digits num))]
+;;               (and (non-decreasing ds)
+;;                    (not-part-of-larger-group ds))))
+;;           (range start (inc end))))
